7c4713d621dcf69dfdb315436d3cb8502e810bff
久松勇斗 <>
2019-09-05 13:54:29 +0900

    LeBronのチェイスダウンブロックで、別クラスのshootResultを減らすように改良

--- LeBronJames.php
+++ LeBronJames.php
@@ -6,7 +6,7 @@ class LeBronJames extends Player
     function ChaseDownBlock($enemy)
     {
         echo 'チェイスダウンブロック!!!<br>';
-        $this->shootResult -= 1;
+        $enemy->shootResult -= 1;
         echo $enemy . 'のシュートをブロックした！';
     }
 }
